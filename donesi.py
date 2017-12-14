06bcd1c483917361d418cd544fb31cce8089811d
Dragan Markovic (E-Search) <v-drmark@microsoft.com>
2017-12-14 12:41:09 +0100

    added more characters into regex for item name

--- donesi.py
+++ donesi.py
@@ -18,43 +18,48 @@ def ExtractItems(raw_data):
   patternPrice = re.compile(r'(\<dd class\=\"price\" itemprop=\"price\"\>)(\d+,?\d+)')
 
   for match in re.findall(patternPrice, raw_data):
+    # print (match)
     prices.append(int(match[1].replace(',', '')))
 
   if not prices:
     patternPrice2 = re.compile(r'(content=\")(\d+,?\d+)([A-Za-z0-9\.\+ ]*\" itemprop=\"price\")')
     for match in re.findall(patternPrice2, raw_data):
+      # print (match)
       prices.append(int(match[1].replace(',', '')))
 
-  patternName = re.compile(r'(.php\"[ ]*itemprop=\"url\">)([A-Za-zČĆĐŠŽžšđćč0-9,\.\!\? ]+)')
+  patternName = re.compile(r'(.php\"[ ]*itemprop=\"url\">)([A-Za-zČĆĐŠŽžšđćč0-9,\.\!\?\"\'\- ]+)')
   for match in re.findall(patternName, raw_data):
+    # print (match)
     names.append(match[1])
 
   if not names:
-    patternName2 = re.compile(r'(modal\"[ ]*itemprop=\"url\">)([A-Za-zČĆĐŠŽžšđćč0-9,\.\!\? ]+)')
+    patternName2 = re.compile(r'(modal\"[ ]*itemprop=\"url\">)([A-Za-zČĆĐŠŽžšđćč0-9,\.\!\?\"\'\- ]+)')
     for match in re.findall(patternName2, raw_data):
+      # print (match)
       names.append(match[1])
 
   return list(zip(prices, names))
 
-assert len(sys.argv) == 4, "Command line arguments are url, price, and range."
+if __name__== "__main__":
+  assert len(sys.argv) == 4, "Command line arguments are url, price, and range."
 
-initalPrice = int(sys.argv[2])
-priceRange = int(sys.argv[3])
+  initalPrice = int(sys.argv[2])
+  priceRange = int(sys.argv[3])
 
-items = ExtractItems(GetSourceFile(sys.argv[1]))
+  items = ExtractItems(GetSourceFile(sys.argv[1]))
 
-combinations = {k:[] for k in range(0, int(sys.argv[2])+1)}
-combinations[0] = [[]]
+  combinations = {k:[] for k in range(0, int(sys.argv[2])+1)}
+  combinations[0] = [[]]
 
-for item in items:
-  for price in range(initalPrice, item[0] - 1, -1):
-    oldPrice = price - item[0]
-    if oldPrice in combinations.keys():
-      for combo in combinations[oldPrice]:
-        combinations[price].append(combo + [item[1]])
+  for item in items:
+    for price in range(initalPrice, item[0] - 1, -1):
+      oldPrice = price - item[0]
+      if oldPrice in combinations.keys():
+        for combo in combinations[oldPrice]:
+          combinations[price].append(combo + [item[1]])
 
-for price in range(max(1, initalPrice - priceRange), initalPrice + 1):
-  if len(combinations[price]) > 0:
-    print (str(price) + ":")
-    for solution in combinations[price]:
-      print ("    " + str(solution))
+  for price in range(max(1, initalPrice - priceRange), initalPrice + 1):
+    if len(combinations[price]) > 0:
+      print (str(price) + ":")
+      for solution in combinations[price]:
+        print ("    " + str(solution))
